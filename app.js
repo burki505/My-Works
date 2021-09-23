c74436845ae9a3d3ef1bd6d469cdbc32554a51ee
burki505 <63101069+burki505@users.noreply.github.com>
2021-09-23 19:41:21 +0300

    node-2

--- app.js
+++ app.js
@@ -4,19 +4,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+// const bcrypt = require("bcrypt");
+
+// const saltRounds = 10;          //hashlanmiş şifrenin gucunu boylece bcrpypt içinde arttırıyoruz
 
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+
 app.use(express.static("public"));
+
+//passport js use section
+app.use(session({
+    secret: "ourlittlesecret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+//passport initialize
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 
 mongoose.connect("mongodb://localhost:27017/userDB",{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false});
 
@@ -27,23 +51,73 @@ mongoose.connect("mongodb://localhost:27017/userDB",{ useUnifiedTopology: true, 
 
 //yukardaki normal seyler için kullanılabilir ama schema fonksiyonlarını kullanacagımız icin assagıdakini kullanmak gerek
 const userSchema = new mongoose.Schema({
-    email : "String",
-    password : "String"
+    email : String,
+    password : String,
+    googleId : String,
+    secret: String
 });
 
-//encryption için secret key olusturuyoruz normalde process.env de vardı.encrpt olacak alanı encrptedFields ile seciyoruz
-//bunları yazdıktan sonra tekrar loginde veya registerda tanımlamaya gerek yok cünku her find veya save fonksiyonu calıstıgında burası calısacak
+//for hash and save the users to the database
+userSchema.plugin(passportLocalMongoose);
 
-userSchema.plugin(encrypt,{secret: process.env.SECRET,encryptedFields : ["password"]});
+//adding findorcreate plugin
+userSchema.plugin(findOrCreate);
+
+//encryption için secret key olusturuyoruz normalde process.env de vardı.encrpt olacak alanı encrptedFields ile seciyoruz
+
 
 //User model
 const User = new mongoose.model("User",userSchema);
+
+//passport documentation npm
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+//google auth
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 
 app.get("/",function(req,res){
     res.render("home");
 }); 
+
+//authentication through google similar with local below
+app.get("/auth/google", passport.authenticate('google', {
+
+    scope: ['profile']
+
+}));
+
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.get("/login",function(req,res){
     res.render("login");
@@ -53,40 +127,154 @@ app.get("/register",function(req,res){
     res.render("register");
 }); 
 
-app.post("/register",function(req,res){
-    const newUser = new User({
-        email : req.body.username,
-        password : req.body.password
-    });
 
-    //if there is no error then we will redirect to secrets page
-    newUser.save(function(err){
-        if(err) res.send(err);
+app.get("/submit",function(req,res){
+
+    if(req.isAuthenticated()){
+        res.render("submit");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
+
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+
+    //passport hides user informations in req so we dont need to initialize again here
+    User.findById(req.user.id,function(err,foundUser){
+        if(err) console.log(err);
+
         else{
-            res.render("secrets");
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                });
+            }
         }
     });
 });
 
+//after getting auth we are here to get.if we are not logged we cant reach it by url /secrets.when we react outh we will search by find method that users have secrets
+app.get("/secrets",function(req,res){
+   User.find({"secret": {$ne: null}},function(err,foundUser){
+    if(err) console.log(err);
+
+    else{
+        if(foundUser){
+            res.render("secrets", {usersWithSecrets: foundUser});
+        }
+    }
+   });
+});
+
+
+
+
+app.get("/logout",function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+app.post("/register",function(req,res){
+
+
+    //user authentication
+    User.register({username: req.body.username},req.body.password,function(err,user){
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
+    });
+
+
+
+    
+    // //password hashlame
+    // bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+    //     const newUser = new User({
+    //         email : req.body.username,
+    //         password : hash
+    //     });
+    
+    //     //if there is no error then we will redirect to secrets page
+    //     newUser.save(function(err){
+    //         if(err) res.send(err);
+    //         else{
+    //             res.render("secrets");
+    //         }
+    //     });
+    // });
+    
+  
+});
+
 app.post("/login",function(req,res){
-    const username = req.body.username;
-    const password = req.body.password;
 
-    User.findOne({email : username},function(err,foundUser){
 
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+
+    req.login(user,function(err){
         if(err){
             console.log(err);
         }
-        //error yoksa kullanıcının varlıgında password sorgulanır eşitse login yapılır
         else{
-            if(foundUser){
-                if(foundUser.password === password){
-                    res.render("secrets");
-                }
-            }
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
-
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const username = req.body.username;
+    // const password = req.body.password;
+
+    // User.findOne({email : username},function(err,foundUser){
+
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     //error yoksa kullanıcının varlıgında password sorgulanır eşitse login yapılır
+    //     else{
+    //         if(foundUser){
+    //             //girdigimiz sifreyle databasedaki şifreyi karsılastırıyozu
+    //             bcrypt.compare(password, foundUser.password, function(err, result) {
+    //                 if(result){
+    //                     res.render("secrets");
+    //                 }
+    //             });
+    //         }
+    //     }
+
+    // });
 });
 
 
